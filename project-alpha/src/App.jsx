79eef4f6a57190e86443807b70b9a90d1939694a
shashankgowda1092 <104762868+Shashankgowda1092@users.noreply.github.com>
2025-05-23 11:52:21 +0000

    hook form

--- project-alpha/src/App.jsx
+++ project-alpha/src/App.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import UserForm from "./pages/reacthookform";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <h1>SHASH</h1>
-     
+
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <h1>SHASH</h1>
+      <UserForm title="Welcome to React Hook Form" count={5} />
+  
     </>
-  )
+  );
 }
 
-export default App
+export default App;
