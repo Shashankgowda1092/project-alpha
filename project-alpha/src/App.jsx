5a540cd3d8dcab39f82666d64c9e87a01c077d67
Shashank Gowda <shashankh1092@gmail.com>
2025-06-07 11:24:41 +0530

    issues fixed

--- project-alpha/src/App.jsx
+++ project-alpha/src/App.jsx
@@ -1,15 +1,12 @@
-import { Button } from "@/components/ui/button"
-import Page from "./app/dashboard/page"
-
-
+import { Button } from "@/components/ui/button";
+import Page from "./app/dashboard/page";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button className='bg-amber-300'>Click me</Button>
       <Page />
     </div>
-  )
+  );
 }
 
 export default App;
